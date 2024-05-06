beece17a93a68201cda54fa2ceac30dbbb304197
Mia <167260010+m1a5leep@users.noreply.github.com>
2024-05-06 21:54:36 +0800

    Added html, css and python file

---
+++ Boost Buddy Project/test.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+@app.route('/task')
+def task():
+    return render_template('task.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
