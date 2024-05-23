522fabffd6309dd4fcaa23a3cd79ef67b09d3f8d
Damia Adlina <nurdamiaadlina05@gmail.com>
2024-05-23 13:33:53 +0800

    Just adding a py file to test things.

--- Boost Buddy Project/test.py
+++ Boost Buddy Project/test.py
@@ -1,10 +1,25 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
 notes = []
 tasks = []
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.string(150), nullable=False)
+    task_description = db.Column(db.text, nullable=False)
+    task_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task('{self.task_name}', '{self.task_description}', '{self.task_date}')"
+    
+class Task(db.Model):
 
 @app.route('/')
 def index():
