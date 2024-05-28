57ca8a183cf68fa4b9b17c689e60b46d025fcc45
nuhafalisha <167696955+nuhafalisha@users.noreply.github.com>
2024-05-28 23:06:45 +0800

    login

--- LOGIN/app.py
+++ LOGIN/app.py
@@ -3,12 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from views import auth_bp
 
 app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+#user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+#database table
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 app.register_blueprint(auth_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- LOGIN/views.py
+++ LOGIN/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, User
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -8,13 +10,39 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
 
-        #registration logic
-
+        #registration logic 
         return redirect(url_for("auth.login"))
+    
+        #hash/cover password
+        hashed_password = generated_password_hash(password, method='sha256')
 
+        #creating new user and add to database
+        new_user = User(username=username, password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("You have registered with Boost Buddy!")
+        return redirect(url_for("auth.login"))
 
     return render_template("register.html")
 
-@auth_bp.route("/login")
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            flash("Login Successful! Welcome back!")
+            return redirect(url_for("index"))
+        else:
+            flash("Invalid username or password! Try Again!")
+
+    return render_template("login.html")
+
+@auth_bp.route("/")
+def index():
     return render_template("login.html")
